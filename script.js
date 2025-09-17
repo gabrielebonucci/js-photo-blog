80014a004d41a46021ab98229bd3fee83bc4160b
Gabriele Bonucci <g.bonucci99@gmail.com>
2025-09-17 15:24:43 +0200

    Chiamata ajax all'api in sviluppo

--- script.js
+++ script.js
@@ -2,9 +2,16 @@
 const cardContainer = document.querySelector('.card-container');
 
 //setto la variabile dell'endpoint
-const apiUrl = 'https://lanciweb.github.io/demo/api/pictures/';
+const apiRef = 'https://lanciweb.github.io/demo/api/pictures/';
 
 //svuoto il contenitore
 cardContainer.innerHTML = '';
 
 //chiamata ajax all'api
+axios.get(apiRef).then((response) => {
+    //estrapoliamo i dati
+    const photos  = response.data;
+    console.log('Dati ricevuti', photos);
+    
+}
+)
